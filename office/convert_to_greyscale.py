194dc16323e8f49e6869a7ceaf73319b0c410d56
florianpfleiderer <florian@pfleiderer.at>
2024-02-16 20:19:37 +0100

    added new configuration scripts

---
+++ office/convert_to_greyscale.py
@@ -0,0 +1,15 @@
+import os
+from PIL import Image
+
+# Durch alle Dateien im aktuellen Verzeichnis iterieren
+for filename in os.listdir('.'):
+    # Nur PNG-Dateien, die "depth" im Namen haben, verarbeiten
+    if 'depth' in filename and filename.endswith('.png'):
+        # Bild laden
+        img = Image.open(filename)
+
+        # Bild in Graustufen konvertieren
+        img_gray = img.convert('L')
+
+        # Graustufenbild speichern
+        img_gray.save(filename)
--- office/create_yml.py
+++ office/create_yml.py
@@ -9,10 +9,17 @@ folder = "office/"
 
 # Liste für die Bildinformationen
 batch = []
+depth_images = []
 
 # Sammeln und sortieren Sie die Dateien im Verzeichnis
 images = sorted([f for f in os.listdir(image_dir) if f.endswith(".png")])
 images.sort(key=lambda x: x[3:5])
+
+# Extrahieren Sie alle Tiefenbilder in eine separate Liste
+depth_images = [image for image in images if 'depth' in image]
+
+# Entfernen Sie die Tiefenbilder aus der images-Liste
+images = list(filter(lambda x: 'depth' not in x, images))
 
 # Gruppieren Sie die Bilder nach Bildnummer
 grouped_images = groupby(images, key=lambda x: x[4])
@@ -20,14 +27,23 @@ grouped_images = groupby(images, key=lambda x: x[4])
 # Durchlaufen Sie die Gruppen und erstellen Sie Paare von Bildern mit unterschiedlichen Szenen
 for _, group in grouped_images:
     group = list(group)
-    # print(group)
     for image1, image2 in zip_longest(group[::2], group[1::2]):
         if image1 and image2:
-            batch.append({
-                "image1": f"{folder}{image1}",
-                "image2": f"{folder}{image2}",
-                "registration_strategy": "2d"
-            })
+            # Überprüfen Sie, ob ein Tiefenbild mit demselben Namen existiert
+            if any(image1[:12] in depth_image for depth_image in depth_images) and any(image2[:12] in depth_image for depth_image in depth_images):
+                batch.append({
+                    "image1": f"{folder}{image1}",
+                    "image2": f"{folder}{image2}",
+                    "depth1": f"{folder}{image1[:11]}_depth.png",
+                    "depth2": f"{folder}{image2[:11]}_depth.png",
+                    "registration_strategy": "3d"
+                })
+            else:
+                batch.append({
+                    "image1": f"{folder}{image1}",
+                    "image2": f"{folder}{image2}",
+                    "registration_strategy": "3d"
+                })
 
 # Schreiben Sie die batch-Liste in die YAML-Datei
 with open("input_metadata.yml", "w") as file:
