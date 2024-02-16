15e2c51723d93d0dde7c2e256a056fceacf691b8
florianpfleiderer <florian@pfleiderer.at>
2024-02-16 16:07:32 +0100

    created yml script

--- office/create_yml.py
+++ office/create_yml.py
@@ -1,9 +1,11 @@
 import os
+from matplotlib.pylab import f
 import yaml
 from itertools import groupby, zip_longest
 
 # Pfad zum Verzeichnis mit den Bildern
 image_dir = "."
+folder = "office/"
 
 # Liste für die Bildinformationen
 batch = []
@@ -11,20 +13,20 @@ batch = []
 # Sammeln und sortieren Sie die Dateien im Verzeichnis
 images = sorted([f for f in os.listdir(image_dir) if f.endswith(".png")])
 images.sort(key=lambda x: x[3:5])
-print(images)
+
 # Gruppieren Sie die Bilder nach Bildnummer
 grouped_images = groupby(images, key=lambda x: x[4])
 
 # Durchlaufen Sie die Gruppen und erstellen Sie Paare von Bildern mit unterschiedlichen Szenen
 for _, group in grouped_images:
     group = list(group)
-    print(group)
+    # print(group)
     for image1, image2 in zip_longest(group[::2], group[1::2]):
         if image1 and image2:
             batch.append({
-                "image1": image1,
-                "image2": image2,
-                "registration_strategy": "3d"
+                "image1": f"{folder}{image1}",
+                "image2": f"{folder}{image2}",
+                "registration_strategy": "2d"
             })
 
 # Schreiben Sie die batch-Liste in die YAML-Datei
