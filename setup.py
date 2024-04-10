792373dc628b6922379e243ffe12a5ddcbb3918c
Florian Pfleiderer <florian@pfleiderer.at>
2024-04-10 12:40:39 +0000

    added setup.py to support packages better

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cyws3d-pipeline',
+    version='0.2',
+    description='A pipeline for 3D object annotation and visualization using cyws3d and the obchange dataset',
+    author='Florian Pfleiderer',
+    author_email='florian@pfleiderer.at',
+    license='MIT',
+    packages=find_packages()
+)
