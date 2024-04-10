9dd2338b52035649894bb2e4c6750cecac511a88
Florian Pfleiderer <florian@pfleiderer.at>
2024-04-10 12:41:09 +0000

    changed foldername to underscore

---
+++ annotation_pipeline/projection.py
@@ -0,0 +1,221 @@
+''' This script contains classes to store intrinsic and extrinsic parameters of a camera and a
+function to project a pointcloud onto 2d coordinates given the intrinsics and extrinsics according
+to the classes defined below.
+'''
+import xml.etree.ElementTree as ET
+import json
+import open3d as o3d
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use('TkAgg')  # or 'Qt5Agg', 'GTK3Agg', etc.
+
+class Intrinsic():
+    ''' Class to store intrinsic parameters of a camera
+
+    Args:
+        cx: principal point x
+        cy: principal point y
+        f: focal length
+    '''
+    def __init__(self, cx=None, cy=None, f=None, width=None, height=None):
+        self.cx = cx
+        self.cy = cy
+        self.f = f
+        self.width = width
+        self.height = height
+        self.distortion = None
+
+    def from_xml(self, xml_file: str):
+        ''' Load the intrinsic parameters from an xml file
+
+        Args:
+            xml_file: relative path to the xml file
+        '''
+        root = ET.parse(xml_file).getroot()
+        calibration = root.find(".//calibration")
+        self.cx = float(calibration.find('cx').text)
+        self.cy = float(calibration.find('cy').text)
+        self.f = float(calibration.find('f').text)
+        self.width = int(root.find(".//resolution").attrib['width'])
+        self.height = int(root.find(".//resolution").attrib['height'])
+
+    def from_json(self, json_file: str):
+        ''' Load the intrinsic parameters from a json file
+
+        Args:
+            json_file: relative path to the json file
+        '''
+        with open(json_file, 'r', encoding='utf-8') as f:
+            temp = json.load(f)
+            data = np.array(temp['K']).reshape(3, 3)
+            self.cx = data[0][2]
+            self.cy = data[1][2]
+            self.f = data[0][0]
+            self.distortion = np.array(temp['D'])
+            self.height = temp['height']
+            self.width = temp['width']
+
+
+    def matrix(self):
+        ''' Return the intrinsic matrix of the camera '''
+        return np.array([[self.f, 0, self.cx],
+                        [0, self.f, self.cy],
+                        [0, 0, 1]])
+
+    def homogenous_matrix(self):
+        ''' Return the homogenous intrinsic matrix of the camera '''
+        return np.array([[self.f, 0, self.cx, 0],
+                        [0, self.f, self.cy, 0],
+                        [0, 0, 1, 0]])
+
+
+class Extrinsic():
+    ''' Class to store extrinsic parameters of a camera
+
+    Args:
+        M: 4x4 homogenous extrinsic matrix
+        position: position of the camera (in world coordinates)
+        rotation: rotation matrix of the camera (in world coordinates)
+    '''
+    def __init__(self, position=None, rotation=None):
+        self.extrinsic_matrix = np.eye(4)
+        self.position = position
+        self.rotation = rotation
+
+    def from_xml(self, xml_file: str):
+        ''' Load the viewpoint from an xml file and calculate the extrinsic matrix
+
+        Args:
+            xml_file: relative path to the xml file
+        '''
+        root = ET.parse(xml_file).getroot()
+        transform = root.find('.//camera[@id="1"]/transform').text
+        self.extrinsic_matrix = np.asarray([transform.split(' ')]).astype(np.float32).reshape(4, 4)
+
+    def from_json(self, json_file: str, scale: int = 1):
+        ''' Load the viewpoint from a json file and calculate the extrinsic matrix
+
+        Args:
+            json_file: relative path to the json file
+        '''
+        with open(json_file, 'r', encoding='utf-8') as f:
+            trajectory = json.load(f)['trajectory'][0]
+
+        lookat = np.asarray(trajectory['lookat'])
+        front = np.asarray(trajectory['front'])
+        up = np.asarray(trajectory['up'])
+        zoom = trajectory['zoom']
+
+        self.position = lookat + front * zoom * scale
+
+        # Normalize the vectors
+        def normalize(v):
+            norm = np.linalg.norm(v)
+            if norm == 0:
+                raise ValueError("Cannot normalize a vector with zero magnitude")
+            return v / norm
+
+        front = normalize(front)
+        up = normalize(up)
+        right = normalize(np.cross(up, front))
+        up = np.cross(front, right)  # 'up' is recalculated to ensure orthogonality
+
+        # Construct the rotation matrix
+        self.rotation = np.column_stack((right, up, front))
+
+        # Construct the rotation matrix
+        self.rotation = np.column_stack((right, up, front))
+
+        self.extrinsic_matrix[:3, :4] = np.column_stack((self.rotation, self.position))
+        self.extrinsic_matrix = np.linalg.inv(self.extrinsic_matrix)
+
+    def matrix(self):
+        ''' Return the extrinsic matrix of the camera '''
+        return self.extrinsic_matrix[:3, :4]
+
+    def homogenous_matrix(self):
+        ''' Return the homogenous extrinsic matrix of the camera '''
+        return self.extrinsic_matrix
+
+
+def inside_frustum(point, fov, near, far):
+    ''' Check if point is inside frustum '''
+    x, y, z = point
+    if z > near or z < far:
+        return False
+    if abs(x) > abs(z * np.tan(np.radians(fov / 2))):
+        return False
+    if abs(y) > abs(z * np.tan(np.radians(fov / 2))):
+        return False
+    return True
+
+
+def frustum_culling(points: np.array, fov: int) -> None:
+    ''' Filter points based on frustum culling 
+    
+    Args:
+        points: np.array of shape (N, 3)
+        fov: field of view
+        
+    Returns:
+        list of filtered points indices
+    '''
+
+    far = min(points[:, 2])
+    near = max(points[:, 2])
+
+    indices = [i for i, point in enumerate(points) if inside_frustum(point, fov, near, far)]
+    return indices
+
+def project_to_2d(points_pos: np.array, K: np.array, width: int = 640, height: int = 480) \
+                  -> (np.array, np.array):
+    ''' use pinhole model to project a pointcloud onto 2d coordinates:
+
+    Args:
+        points_pos: Nx3 array of 3d points in camera coordinate system
+        K: 3x4 homogenous intrinsic matrix
+            | fx 0  cx 0 |
+            | 0  fy cy 0 |
+            | 0  0  1  0 |
+        width: width of the image in pixels
+        height: height of the image in pixels
+    
+    Returns:
+        u: x coordinates in image plane
+        v: y coordinates in image plane
+    '''
+    assert points_pos.shape[1] == 3, "points_pos must be Nx3"
+    assert K.shape == (3, 4), "K must be 3x4"
+
+    # homogenous coordinates
+    # hom_coord = np.hstack(((points_pos / np.kron(points_pos[:, 2], np.ones((3, 1))).T), np.ones((points_pos.shape[0], 1))))
+    hom_coord = np.hstack((points_pos, np.ones((points_pos.shape[0], 1))))
+
+    # projection
+    proj = K @ hom_coord.T
+    u = np.round(proj[0, :] / proj[2, :]).astype(int).clip(0, width-1)
+    v = np.round(proj[1, :] / proj[2, :]).astype(int).clip(0, height-1)
+
+    return u, v
+
+
+if __name__ == "__main__":
+    # load pcd file
+    pcd_name: str = "./testmodel/model1.pcd"
+    pcd = o3d.io.read_point_cloud(pcd_name)
+
+    # Build intrinsic matrix
+    intrinsics = Intrinsic()
+    intrinsics.from_xml("./testmodel/model1_cameras.xml")
+
+    K = intrinsics.homogenous_matrix()
+    print(K)
+
+    # extrinsic matrix
+    extrinsics = Extrinsic()
+    extrinsics.from_json("./testmodel/model1_viewpoint_00.json", scale = 10)
+    M = extrinsics.homogenous_matrix()
+    print(M)
+
---
+++ annotation_pipeline/utils.py
@@ -0,0 +1,154 @@
+''' utility functions for point cloud processing and obchange dataset
+'''
+import copy
+import open3d as o3d
+import cv2
+import numpy as np
+from .projection import Intrinsic
+
+def read_annotation(path_to_annotation: str) -> dict:
+    ''' Read annotation file and return dictionary '''
+    with open (path_to_annotation, "r", encoding="utf-8") as f:
+        anno = f.readlines()
+        annotation_dictionary = {}
+        for line in anno:
+            line = line.split()
+            obj_name = line[0]
+            indices = line[1:-1]
+            indices = [int(i) for i in indices]
+            annotation_dictionary[obj_name] = indices
+    return annotation_dictionary
+
+
+def annotate_pcd(input_pcd: o3d.geometry.PointCloud, \
+                 path_to_annotation: str, \
+                 gt_colour: np.array = np.array([0.1, 0.9, 0.1])) \
+                     -> (o3d.geometry.PointCloud, dict):
+    ''' Annotate point cloud with indices from annotation file '''
+    annotated_pcd = copy.deepcopy(input_pcd)
+    with open (path_to_annotation, "r", encoding="utf-8") as f:
+        anno = f.readlines()
+        annotation_dictionary = {}
+        for line in anno:
+            line = line.split()
+            obj_name = line[0]
+            indices = line[1:-1]
+            indices = [int(i) for i in indices]
+            annotation_dictionary[obj_name] = indices
+
+    for _, indices in annotation_dictionary.items():
+        color = gt_colour
+        for i in indices:
+            annotated_pcd.colors[i] = color
+    return annotated_pcd, annotation_dictionary
+
+
+def extract_3d_bboxes(input_pcd: o3d.geometry.PointCloud, \
+                        annotation_dictionary: dict, \
+                        result=False) -> list:
+    ''' Draw bounding boxes around annotated objects 
+    
+    Args:
+        pcd: point cloud
+        annotation_dictionary: dictionary of annotations
+
+    Returns:
+        list of bounding boxes
+    '''
+    bboxes = []
+    for obj in annotation_dictionary:
+        indices = annotation_dictionary[obj]
+        points = np.asarray(input_pcd.points)[indices]
+        bbox = o3d.geometry.AxisAlignedBoundingBox.\
+            create_from_points(o3d.utility.Vector3dVector(points))
+        bboxes.append(bbox)
+        if result:
+            o3d.visualization.draw_geometries([input_pcd, bbox])
+    return bboxes
+
+
+def draw_2d_bboxes(pixel_coordinates: tuple, \
+                   points_color: np.array, \
+                   gt_coordinates: tuple, intrinsics: Intrinsic):
+    ''' Draw 2D bounding boxes on image 
+    
+    Args:
+        pixel_coordinates: tuple of u, v coordinates
+        points_color: np.array of colors
+        gt_coordinates: tuple of gt_u, gt_v coordinates
+        intrinsics: object of instance Intrinsic
+        
+    Returns:
+        cv2 image with bounding boxes
+    '''
+    u_coords, v_coords = pixel_coordinates
+    gt_u, gt_v = gt_coordinates
+
+    assert len(gt_u) == len(gt_v) != 0, "gt_object not in fov"
+    assert isinstance(intrinsics ,Intrinsic), f"intrinsics must be instance of Intrinsic Class,\
+                                                but is {type(intrinsics)}"
+
+    image = np.zeros((intrinsics.height, intrinsics.width, 3), dtype=np.uint8)
+    points_color = points_color * 255
+    # Fill the image array with your colors at the corresponding u, v coordinates
+    for u, v, color in zip(u_coords, v_coords, points_color):
+        image[int(v), int(u)] = color
+
+    if image.shape[2] == 3:  # Convert RGB to BGR for OpenCV if necessary
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    top_left = (min(gt_u), min(gt_v))
+    bottom_right = (max(gt_u), max(gt_v))
+
+    # Define the color and thickness of the bounding box
+    color = (0, 255, 0)
+    thickness = 2
+
+    cv2.rectangle(image, top_left, bottom_right, color, thickness)
+
+    return cv2.flip(image, 1)
+
+
+def draw_2d_bboxes_on_img(image_path: str, gt_u, gt_v):
+    ''' Draw 2D bounding boxes on image
+
+    Args:
+        image_path: path to image
+        gt_u: list of x coordinates for ground truth
+        gt_v: list of y coordinates for ground truth
+
+    Returns:
+        image with bounding boxes
+    '''
+
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.flip(image, 1)
+
+    min_u, max_u = min(gt_u), max(gt_u)
+    min_v, max_v = min(gt_v), max(gt_v)
+
+    top_left = (min_u, min_v)
+    bottom_right = (max_u, max_v)
+
+    # Define the color and thickness of the bounding box
+    color = (0, 255, 0)  # Green for visibility
+    thickness = 2  # Thickness of the box lines
+
+    # Draw the rectangle
+    cv2.rectangle(image, top_left, bottom_right, color, thickness)
+
+    return cv2.flip(image, 1)
+
+
+if __name__ == "__main__":
+    PCD_NAME: str = "../data/annotation/office/scene4/merged_plane_clouds_ds002.pcd"
+    # PCD_NAME: str = "./testmodel/model1.pcd"
+    ANNO_NAME: str = "../data/annotation/office/scene4/merged_plane_clouds_ds002_GT.anno"  
+        
+    pcd = o3d.io.read_point_cloud(PCD_NAME)
+    print(pcd)
+    mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1, origin=[0, 0, 0])
+    pcd, annotation_dict = annotate_pcd(pcd, ANNO_NAME)
+
+    o3d.visualization.draw_geometries([pcd, mesh_frame])
