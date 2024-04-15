0179ffa260d9a517004ad2ec060dc708e165df83
Florian Pfleiderer <florian@pfleiderer.at>
2024-04-15 11:48:30 +0000

    added map test to testrunner

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,6 +1,7 @@
 import unittest
-from testunit import test_projection
+from testunit import test_projection, test_evaluation
 
 testsuite = unittest.TestLoader().loadTestsFromModule(test_projection)
+testsuite.addTests(unittest.TestLoader().loadTestsFromModule(test_evaluation))
 runner = unittest.TextTestRunner()
 runner.run(testsuite)
