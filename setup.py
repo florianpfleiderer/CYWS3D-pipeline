c9b567b4e435b6742d49dd16c5fda25499befc18
Florian Pfleiderer <florian@pfleiderer.at>
2024-04-17 15:09:50 +0000

    updated version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cyws3d-pipeline',
-    version='0.2',
+    version='0.4',
     description='A pipeline for 3D object annotation and visualization using cyws3d and the obchange dataset',
     author='Florian Pfleiderer',
     author_email='florian@pfleiderer.at',
