f8b8dd01d655d9079e7114a8633848e8c07e6142
florianpfleiderer <florian@pfleiderer.at>
2024-04-23 18:45:15 +0000

    updated version to 1.0

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='cyws3d-pipeline',
-    version='0.7',
+    version='1.0',
     description='A pipeline for 3D object annotation and visualization using cyws3d and the obchange dataset',
     author='Florian Pfleiderer',
     author_email='florian@pfleiderer.at',
     license='MIT',
     packages=find_packages(),
     scripts=['scripts/annotate.py', 'scripts/inference.py', 'scripts/run_tests.py', \
-        'scripts/create_inference_metadata.py'],
+        'scripts/create_inference_metadata.py', 'scripts/view_pt.py']
 )
