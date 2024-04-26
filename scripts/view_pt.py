7baec51b41b2dd612622a86e3da96510828e3290
Florian Pfleiderer <florian@pfleiderer.at>
2024-04-26 14:38:22 +0000

    minor changes

--- scripts/view_pt.py
+++ scripts/view_pt.py
@@ -1,7 +1,15 @@
 import torch
+import numpy as np
 
-print(torch.cuda.is_available())
-print(torch.cuda.get_device_name(0))
+positions = np.load("data/inference/demo_data/position1.npy")
+rotations = np.load("data/inference/demo_data/rotation1.npy")
+intrinsics = np.load("data/inference/demo_data/intrinsics1.npy")
+print(f"positions: {positions}")
+print(f"\nrotations: {rotations}")
+print(f"\nintrinsics: {intrinsics}")
 
+
+# print("target bboxes:\n")
 # print(torch.load("data/GH30_Office/all_target_bboxes.pt"))
+# print("\n\npredictions:\n")
 # print(torch.load("data/GH30_Office/predictions/batch_image2_predicted_bboxes.pt"))
