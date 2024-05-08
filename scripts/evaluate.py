111d7702034e3e80e1a72fc5e7a3bb147dfa4688
Florian Pfleiderer <florian@pfleiderer.at>
2024-05-08 13:19:37 +0000

    refactored code

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -4,7 +4,6 @@
 Insert Module Description Here
 """
 import os
-import argparse
 import yaml
 import logging
 import torch
@@ -13,127 +12,54 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 import numpy as np
 from torchmetrics.detection.mean_ap import MeanAveragePrecision
-from src.evaluation_pipeline.calculate_mAP import calculate_mAP
+from src.evaluation_pipeline import eval_utils, eval_plotter
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig()
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--room", required=True, help = "name of room (e.g. Office)")
-args = vars(parser.parse_args())
-ROOM_DIR = f"data/GH30_{args['room']}"
-PREDICTIONS_DIR = ROOM_DIR+"/predictions/batch_image2_predicted_bboxes.pt"
-TARGET_BBOXES_DIR = ROOM_DIR+"/all_target_bboxes.pt"
-preds = torch.load(PREDICTIONS_DIR)
-targets = torch.load(TARGET_BBOXES_DIR)
-iou_thresholds = np.arange(0.3, 0.95, 0.05).tolist()
-rec_thresholds = np.arange(0.1, 1.0, 0.1).tolist()
-max_detection_thresholds = [1, 3, 5]
+def main(
+    room: str = None,
+    log_level: str = "INFO"
+):
+    """
+    main function for evaluation pipeline
+    """
+    logger.setLevel(getattr(logging, log_level.upper()))
+    logger.warning("logger set to %s", logger.level)
 
-metric = MeanAveragePrecision(box_format='xyxy', iou_type='bbox', extended_summary=True, \
-    iou_thresholds=iou_thresholds, rec_thresholds=rec_thresholds, \
-        max_detection_thresholds=max_detection_thresholds)
-metric.update(preds, targets)
-mAP = metric.compute()
+    if room is None:
+        raise ValueError("Please provide a room to evaluate")
+    ROOM_DIR = f"data/GH30_{room}"
+    PREDICTIONS_DIR = ROOM_DIR+"/predictions/batch_image2_predicted_bboxes.pt"
+    TARGET_BBOXES_DIR = ROOM_DIR+"/all_target_bboxes.pt"
 
-for key, value in mAP.items():
-    if isinstance(value, torch.Tensor):
-        mAP[key] = value.squeeze().cpu().numpy()
+    iou_thresholds: np.ndarray = np.arange(0.3, 0.95, 0.05).tolist()
+    rec_thresholds: np.ndarray = np.arange(0.1, 1.0, 0.1).tolist()
+    max_detection_thresholds: list = [1, 3, 5]
 
-fig1, axs = plt.subplots(3, 1, figsize=(10, 10))
-fig1.suptitle(f"precision for {args['room']}")
+    preds = torch.load(PREDICTIONS_DIR)
+    targets = torch.load(TARGET_BBOXES_DIR)
 
-# precision vs IoU at different bbox sizes
-areas = ['all', 'small', 'medium', 'large']
-colors = ['blue', 'green', 'orange', 'red']
+    sorted_targets = eval_utils.prepare_target_bboxes(targets, f"{ROOM_DIR}/input_metadata.yaml")
 
-for i, area in enumerate(areas):
-    precision_values = mAP['precision'][:, 0, i, 0]
-    # print(precision_values)
-    precision_values = [p if p > 0 else 0 for p in precision_values]
-    axs[0].plot(iou_thresholds, precision_values, label=area, color=colors[i])
+    metric = MeanAveragePrecision(box_format='xyxy', iou_type='bbox', extended_summary=True, \
+        iou_thresholds=iou_thresholds, rec_thresholds=rec_thresholds, \
+            max_detection_thresholds=max_detection_thresholds)
+    metric.update(preds, targets)
+    mAP = metric.compute()
 
-axs[0].set_xlabel('IoU Threshold')
-axs[0].set_ylabel('Precision')
-axs[0].set_title('Precision vs IoU Threshold for different bbox sizes')
-axs[0].legend()
+    eval_utils.map_to_numpy(mAP)
 
-# precision vs IoU at different recall thresholds
-for i, rec in enumerate(rec_thresholds):
-    precision_values = mAP['precision'][:, i, 0, 0]
-    precision_values = [p if p > 0 else 0 for p in precision_values]
-    axs[1].plot(iou_thresholds, precision_values, label=rec)
+    eval_plotter.plot_precision(mAP, (iou_thresholds, rec_thresholds, max_detection_thresholds), \
+        room, ROOM_DIR)
 
-axs[1].set_xlabel('IoU Threshold')
-axs[1].set_ylabel('Precision')
-axs[1].set_title('Precision vs IoU Threshold for different recall thresholds')
-axs[1].legend()
+    eval_plotter.plot_recall(mAP, (iou_thresholds, rec_thresholds, max_detection_thresholds), \
+        room, ROOM_DIR)
 
-# precision over IoU at different max detection thresholds
-for i, max_det in enumerate(max_detection_thresholds):
-    precision_values = mAP['precision'][:, 0, 0, i]
-    precision_values = [p if p > 0 else 0 for p in precision_values]
-    axs[2].plot(iou_thresholds, precision_values, label=max_det)
+    eval_utils.save_map_as_json(mAP, f"{ROOM_DIR}/mAP_{room}.json")
 
-axs[2].set_xlabel('IoU Threshold')
-axs[2].set_ylabel('Precision')
-axs[2].set_title('Precision vs IoU Threshold for different max detection thresholds')
-axs[2].legend()
 
-plt.tight_layout()
-fig1.savefig(f"{ROOM_DIR}/precision_{args['room']}.png")
+if __name__ == "__main__":
+    from jsonargparse import CLI
 
-fig2, axs = plt.subplots(3, 1, figsize=(10, 10))
-fig2.suptitle(f"recall for {args['room']}")
-
-# recall vs IoU at different bbox sizes
-areas = ['all', 'small', 'medium', 'large']
-colors = ['blue', 'green', 'orange', 'red']
-
-for i, area in enumerate(areas):
-    recall_values = mAP['recall'][:, i, 0]
-    recall_values = [r if r > 0 else 0 for r in recall_values]
-    axs[0].plot(iou_thresholds, recall_values, label=area, color=colors[i])
-
-axs[0].set_xlabel('IoU Threshold')
-axs[0].set_ylabel('Recall')
-axs[0].set_title('Recall vs IoU Threshold for different bbox sizes')
-axs[0].legend()
-
-# recall vs IoU at different max detection thresholds
-for i, max_det in enumerate(max_detection_thresholds):
-    recall_values = mAP['recall'][:, 0, i]
-    recall_values = [r if r > 0 else 0 for r in recall_values]
-    axs[1].plot(iou_thresholds, recall_values, label=max_det)
-
-axs[1].set_xlabel('IoU Threshold')
-axs[1].set_ylabel('Recall')
-axs[1].set_title('Recall vs IoU Threshold for different max detection thresholds')
-axs[1].legend()
-
-plt.tight_layout()
-fig2.savefig(f"{ROOM_DIR}/recall_{args['room']}.png")
-
-for key, value in mAP.items():
-    if isinstance(value, np.ndarray):
-        mAP[key] = value.tolist()
-
-mAP_json = dict(
-    map=mAP['map'],
-    map_50=mAP['map_50'],
-    map_75=mAP['map_75'],
-    map_small=mAP['map_small'],
-    map_medium=mAP['map_medium'],
-    map_large=mAP['map_large'],
-    mar_1=mAP['mar_1'],
-    mar_3=mAP['mar_3'],
-    mar_5=mAP['mar_5'],
-    mar_small=mAP['mar_small'],
-    mar_medium=mAP['mar_medium'],
-    mar_large=mAP['mar_large']
-)
-
-with open(f"{ROOM_DIR}/mAP_{args['room']}.json", 'w') as f:
-    json.dump(mAP_json, f)
-
+    CLI(main)
